f4d227f19b18aeff37ff9564ff3c39e3a487e7c8
Francisco Morales Sánchez <fcomorales.sanchez@gmail.com>
2021-11-28 02:07:07 -0300

    Create ArticleResource.php

---
+++ app/Http/Resources/ArticleResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ArticleResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'type' => 'articles',
+            'id' => (string) $this->resource->getRouteKey(),
+            'attributes' => [
+                'title' => $this->resource->title,
+                'slug' => $this->resource->slug,
+                'content' => $this->resource->content
+            ],
+            'self' => route('api.v1.articles.show', $this->resource)
+        ];
+    }
+}
