0cc9dc38e3da9208ce301d966e9987932d94ecee
Francisco Morales Sánchez <fcomorales.sanchez@gmail.com>
2021-12-19 12:40:48 -0300

    validate json errors

--- tests/Feature/Articles/CreateArticleTest.php
+++ tests/Feature/Articles/CreateArticleTest.php
@@ -45,4 +45,95 @@ class CreateArticleTest extends TestCase
            ]
         ]);
     }
+
+    /** @test */
+    public function title_is_required()
+    {
+        $response = $this->postJson(route('api.v1.articles.store'), [
+            'data' => [
+                'type' => 'articles',
+                'attributes' => [
+                    'slug' => 'nuevo-articulo',
+                    'content' => 'Contenido del artículo',
+                ]
+            ]
+        ]); //se puede usar ->dump() para ver detalle de error
+        /*se espera un error de validacion en el campo title*/
+        $response->assertJsonValidationErrors('data.attributes.title');
+        /*$article = Article::first();*/
+        /*
+             * $response->assertHeader(
+                'Location',
+                route('api.v1.articles.show', $article)
+            );
+        */
+        /*
+             * $response->assertExactJson([
+                'data' => [
+                    'type' => 'articles',
+                    'id' => (string) $article->getRouteKey(),
+                    'attributes' => [
+                        'title' => 'Nuevo artículo',
+                        'slug' => 'nuevo-articulo',
+                        'content' => 'Contenido del artículo',
+                    ],
+                    'links' => [
+                        'self' => route('api.v1.articles.show', $article)
+                    ]
+                ]
+            ]);
+        */
+    }
+
+    /** @test */
+    public function title_must_be_at_least_4_characters()
+    {
+        $response = $this->postJson(route('api.v1.articles.store'), [
+            'data' => [
+                'type' => 'articles',
+                'attributes' => [
+                    'title' => 'Nue',
+                    'slug' => 'nuevo-articulo',
+                    'content' => 'Contenido del artículo',
+                ]
+            ]
+        ]); //se puede usar ->dump() para ver detalle de error
+        /*se espera un error de validacion en el campo title*/
+        $response->assertJsonValidationErrors('data.attributes.title');
+
+    }
+
+    /** @test */
+    public function slug_is_required()
+    {
+        $response = $this->postJson(route('api.v1.articles.store'), [
+            'data' => [
+                'type' => 'articles',
+                'attributes' => [
+                    'title' => 'Nuevo articulo',
+                    'content' => 'Contenido del artículo',
+                ]
+            ]
+        ]); //se puede usar ->dump() para ver detalle de error
+        /*se espera un error de validacion en el campo title*/
+        $response->assertJsonValidationErrors('data.attributes.slug');
+
+    }
+
+    /** @test */
+    public function content_is_required()
+    {
+        $response = $this->postJson(route('api.v1.articles.store'), [
+            'data' => [
+                'type' => 'articles',
+                'attributes' => [
+                    'title' => 'Nuevo articulo',
+                    'slug' => 'nuevo-articulo',
+                ]
+            ]
+        ]); //se puede usar ->dump() para ver detalle de error
+        /*se espera un error de validacion en el campo title*/
+        $response->assertJsonValidationErrors('data.attributes.content');
+
+    }
 }
