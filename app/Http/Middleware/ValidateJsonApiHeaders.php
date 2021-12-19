71deb66200f5a6911ebe4fb27e15ddb12461b271
Francisco Morales Sánchez <fcomorales.sanchez@gmail.com>
2021-12-19 10:38:44 -0300

    Update middleware

--- app/Http/Middleware/ValidateJsonApiHeaders.php
+++ app/Http/Middleware/ValidateJsonApiHeaders.php
@@ -20,13 +20,17 @@ class ValidateJsonApiHeaders
     public function handle(Request $request, Closure $next)
     {
         if ($request->header('accept') !== 'application/vnd.api+json') {
-            throw new HttpException(406);
+            throw new HttpException(406, __('Not acceptable'));
         }
 
-        if ($request->isMethod('POST')) {
+        if ($request->isMethod('POST') || $request->isMethod('PATCH')) {
             if ($request->header('content-type') !== 'application/vnd.api+json') {
-                throw new HttpException(415);
+                throw new HttpException(415, ('Unsupported Media Type'));
             }
         }
+
+        return $next($request)->withHeaders([
+           'content-type' => 'application/vnd.api+json'
+        ]);
     }
 }
