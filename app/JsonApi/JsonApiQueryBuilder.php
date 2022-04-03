e0c956f7171732e37041d274b02b062dcf6b3a59
Francisco Morales Sánchez <franticzero@gmail.com>
2022-04-03 13:55:01 -0400

    Create JsonApiQueryBuilder.php

---
+++ app/JsonApi/JsonApiQueryBuilder.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\JsonApi;
+
+use Blueprint\Builder;
+use Closure;
+use Illuminate\Support\Str;
+
+class JsonApiQueryBuilder
+{
+    public function allowedSorts(): Closure
+    {
+        return function ($allowedSorts){
+            /** @var Builder $this */
+            if (request()->filled('sort')) {
+                $sortFields = explode(',', request()->input('sort'));
+
+                $allowedSorts = ['title', 'content'];
+
+                foreach ($sortFields as $sortField) {
+                    $sortDirection = Str::of($sortField)->startsWith('-') ? 'desc' : 'asc';
+
+                    $sortField = ltrim($sortField, '-');
+
+                    abort_unless(in_array($sortField, $allowedSorts), 400);
+                    /** @var Builder $this */
+                    $this->orderBy($sortField, $sortDirection);
+                }
+            }
+
+            return $this;
+        };
+    }
+
+    public function allowedFilters(): Closure
+    {
+        return function ($allowedFilters) {
+            /** @var Builder $this */
+            foreach (request('filter', []) as $filter => $value) {
+                dump($filter); //year
+                dump($value);
+                abort_unless(in_array($filter, $allowedFilters), 400);
+
+                $this->hasNamedScope($filter)
+                    ? $this->{$filter}($value)
+                    : $this->where($filter,'LIKE', '%'.$value.'%');
+
+            }
+            /** retornar el builder para seguir encadenando metodos. */
+            return $this;
+        };
+    }
+    public function jsonPaginate(): Closure
+    {
+        return function () {
+            /** @var Builder $this */
+            return $this->paginate(
+                $perPage = request('page.size', 15),
+                $columns = ['*'],
+                $pageName = 'page[number]',
+                $page = request('page.number', 1)
+            )->appends(request()->only('sort', 'filter','page.size'));
+        };
+    }
+
+}
