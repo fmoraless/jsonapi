d9cbadf82a695727a3273e571f99763842825a48
Francisco Morales Sánchez <franticzero@gmail.com>
2022-04-03 13:55:23 -0400

    Update MakesJsonApiRequests.php

--- tests/MakesJsonApiRequests.php
+++ tests/MakesJsonApiRequests.php
@@ -38,10 +38,6 @@ trait MakesJsonApiRequests
     {
         parent::setUp();
 
-        TestResponse::macro(
-            'assertJsonApiValidationErrors',
-            $this->assertJsonApiValidationErrors()
-        );
     }
 
     public function withoutJsonApiDocumentFormatting()
@@ -71,46 +67,4 @@ trait MakesJsonApiRequests
         return parent::patchJson($uri, $data, $headers);
     }
 
-    /**
-     * @return Closure
-     */
-    protected function assertJsonApiValidationErrors(): Closure
-    {
-        return function ($attribute) {
-            /** @var TestResponse $this */
-
-             $pointer = Str::of($attribute)->startsWith('data')
-                ? "/".str_replace('.', '/', $attribute)
-                : "/data/attributes/{$attribute}";
-
-            try {
-                $this->assertJsonFragment([
-                    'source' => ['pointer' => $pointer ]
-                ]);
-            }catch (ExpectationFailedException $e) {
-                PHPUnit::fail(
-                    "Failed to find a JSON:API validation error for key: '{$attribute}'"
-                    .PHP_EOL.PHP_EOL.
-                    $e->getMessage()
-                );
-            }
-            try {
-                $this->assertJsonStructure([
-                    'errors' => [
-                        ['title', 'detail', 'source' => ['pointer']]
-                    ]
-                ]);
-            } catch (ExpectationFailedException $e) {
-                PHPUnit::fail(
-                    "Failed to find a valid JSON:API error response"
-                    .PHP_EOL.PHP_EOL.
-                    $e->getMessage()
-                );
-            }
-
-            $this->assertHeader(
-                'content-type', 'application/vnd.api+json'
-            )->assertStatus(422);
-        };
-    }
 }
