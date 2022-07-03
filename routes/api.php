bfd220b6a79264d64aa1d156b0bf9b983703cb91
Francisco Morales Sánchez <fcomorales.sanchez@gmail.com>
2022-07-02 23:00:49 -0400

    Author resource implementation

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\ArticleController;
+use App\Http\Controllers\Api\AuthorController;
 use App\Http\Controllers\Api\CategoryController;
 use Illuminate\Support\Facades\Route;
 
@@ -21,6 +22,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::apiResource('articles',ArticleController::class);
 Route::apiResource('categories',CategoryController::class)
+    ->only('index', 'show');
+Route::apiResource('authors',AuthorController::class)
     ->only('index', 'show');
 
 Route::get('articles/{article}/relationships/category', fn() => 'TODO')
