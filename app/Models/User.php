a71d8e8fdcfa5b6fa6c89104acad25593625cf68
fmoraless <franticzero@gmail.com>
2023-08-15 11:06:31 -0400

    feat(permissions): create permissions system for API

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,14 @@ class User extends Authenticatable
 
     public $resourceType = 'authors';
 
+    public function permissions()
+    {
+        return $this->belongsToMany(Permission::class);
+    }
+
+    public function givePermissionTo(Permission $permission)
+    {
+        $this->permissions()->syncWithoutDetaching($permission);
+    }
+
 }
